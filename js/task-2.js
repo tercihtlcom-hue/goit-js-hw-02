a8620196aa5bc7e38d29140abf48d9935873fc75
Arif Yazici <tercihtl.com@gmail.com>
2026-03-31 02:00:50 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,13 @@
 function formatMessage(message, maxLength) {
-    if (message.length <= maxLength) {
-        return message;
-    } else {
-        return message.slice(0, maxLength) + "...";
-    }
-
+  // Eğer mesajın uzunluğu maxLength'e eşit veya küçükse mesajın tamamını döndür
+  if (message.length <= maxLength) {
+    return message;
+  } else {
+    // Mesaj uzunsa, maxLength kadar kes ve sonuna "..." ekle
+    return message.slice(0, maxLength) + "...";
+  }
 }
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
